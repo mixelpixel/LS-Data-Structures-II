fdb919d66b45755f7859395743e180cf717144cc
mixelpixel <mixelpix@mac.com>
2017-07-19 12:47:40 -0400

    passes test 1

--- src/graph.js
+++ src/graph.js
@@ -3,6 +3,18 @@ class Graph {
   constructor() {
     this.graph = {};
   }
+  // should have methods named `addNode`, `contains`, `removeNode`, `addEdge`, `getEdge`, and `removeEdge`
+  addNode(newNode, toNode) {
+    this.graph.push({
+      newNode,
+      toNode
+    });
+  }
+  contains(hasValue) {}
+  removeNode(deleteValue) {}
+  addEdge(fromNode, toNode) {}
+  getEdge(fromNode, toNode) {}
+  removeEdge(fromNode, toNode) {}
 }
 
 module.exports = Graph;
